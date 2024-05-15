d189c7e298d9d09e6b503b0c27a8cd73ad457416
Alef Carvalho <alef_ps3@outlook.com>
2024-05-15 20:05:51 -0300

    add color style, mybutton e login

---
+++ lib/assets/src/componentes/color_style.dart
@@ -0,0 +1,10 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class ColorStyle{
+  static const primary =  const Color.fromRGBO(0, 0, 0, 1);
+  static const secondary =  const Color.fromRGBO(0, 0, 0, 1);
+  static const Background = Color.fromARGB(240, 0, 0, 0);
+
+}
