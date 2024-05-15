c6fc9afd7d2ca087d108a87852ae51e34f70c1c9
Alef Carvalho <alef_ps3@outlook.com>
2024-05-15 20:13:22 -0300

    altera pasta de src

--- lib/src/pages/splashscreen.dart
+++ lib/src/pages/splashscreen.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+
+class Splashscreen extends StatefulWidget{
+  const Splashscreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _MyWidgetState();
+  State<Splashscreen> createState() => _MyWidegetState();
 }
 
-class _MyWidgetState extends State<SplashScreen> {
+class _MyWidegetState extends State<Splashscreen> {
   @override
-  Widget build(BuildContext context) {
-    return s(
-      backgroundColor: const Color.fromARGB(0, 0, 0, 1),
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
       body: Center(
-        child: Image.asset('lib/assents/logoApp.png'),
-      )
+        child: Image.asset('lib/assets/logoApp.png'),
+      ),
     );
-  }
+  } 
 }
